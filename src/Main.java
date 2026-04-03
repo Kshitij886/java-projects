55a965f2ff1070361acd4705d413743a12f6611e
Kshitij886 <kshitijkhatri437@gmail.com>
2026-04-03 12:00:07 +0545

    variables and taking input in java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,38 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        // Print out to console
         System.out.println("Hello world");
-//        for (int i = 0; i <= 5; i++){
-//            IO.println("i = " + i);
-//        }
         System.out.println("I like pizza, Buy me a pizza");
+        // Variables
+        // there are two types of variables primitive and non-primitive
+        // primitives are those variables which value store on stack (this contains less storage but fast) Eg: int, char, boolean, double
+        // non-primitives are those variables which value store on heap (large storage but slower than stack) Eg: string, array, objects
+        // creating a variables
+        int a = 30;
+        System.out.println("var is " + a);
+
+        double pi = 3.14;
+
+        char grade = 'A';
+        char symbol = '!';
+
+        boolean isStudent = true;
+        boolean isForSale = false;
+
+        // strings
+         String name  = "Kshitij khatri";
+         System.out.print("Hello "+ name);
+
+         // taking input from console
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String myName = scanner.nextLine();
+        System.out.println("Hello "+myName);
+
+        scanner.close();
+
     }
 }
