ca2578ee18ab7230fad83dbec666282114b4b5ee
Kshitij886 <kshitijkhatri437@gmail.com>
2026-04-03 11:02:32 +0545

    hello world program in java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
-void main() {
-    IO.println("Hello world");
-    for (int i = 0; i <= 5; i++) {
-        IO.println("i = " + i);
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+//        for (int i = 0; i <= 5; i++){
+//            IO.println("i = " + i);
+//        }
+        System.out.println("I like pizza, Buy me a pizza");
     }
 }
